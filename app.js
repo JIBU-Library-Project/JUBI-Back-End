cf34b24ec392336b517f6bcade7d5dfc02e2b57f
Jessica Delali Katsekpor <jessicadelali@gmail.com>
2025-06-11 16:29:55 +0000

    CORS origin modified.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const booksRouter = require("./routes/booksRoute");
 app.use(express.json());
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
-const cors = require("cors");
+app.use(cors({ origin: "*" }));
 
 app.use("/", booksRouter);
-app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 [];
 app.listen(7000, () => {
